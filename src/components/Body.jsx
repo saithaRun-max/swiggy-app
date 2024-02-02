42e74119456dc33454cca17f546562419766b75f
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-02-02 11:53:07 +0530

    day 5

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -63,12 +63,18 @@ const Body = () => {
         </button>
       </div>
       <div className="card-container">
-        {(searchText.length === 0 ? allRestaurents : filterdrestarent).map((restaurent) => {
-          return (
-            <div>
-           <Link to={"/restaurent/" + restaurent.info.id}
-                key={restaurent.info.id} >
-                <RestaurentCard {...restaurent.info} key={restaurent.info.id} />
+        {(searchText.length === 0 ? allRestaurents : filterdrestarent).map(
+          (restaurent) => {
+            return (
+              <div>
+                <Link
+                  to={"/restaurent/" + restaurent.info.id}
+                  key={restaurent.info.id}
+                >
+                  <RestaurentCard
+                    {...restaurent.info}
+                    key={restaurent.info.id}
+                  />
                 </Link>
               </div>
             );
