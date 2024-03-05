2324f5ffeca9e34033a1061c57623b5ea5cbdc03
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-03-05 21:54:51 +0530

    updated

--- src/components/RestaurentCard.jsx
+++ src/components/RestaurentCard.jsx
@@ -4,8 +4,12 @@ import { IMG_CDN_URL } from "./constants";
 const RestaurentCard = (props) => {
   const { name, cloudinaryImageId, cuisines, locality, areaName } = props;
 
+
+ 
+ const filteredCusines = cuisines.length < 4 ? cuisines : cuisines.slice(0, 3);
+
   return (
-    <div className=" min-h-80 max-w-60 m-5 mt-6 shadow-xl rounded-lg p-1 pb-3 ">
+    <div className=" min-h-[350px] max-w-60 m-5 mt-6 shadow-xl rounded-lg p-1 pb-3 ">
       {
         <img
           className="h-40 w-fit  rounded-lg"
@@ -16,7 +20,7 @@ const RestaurentCard = (props) => {
 
       <h4 className="text-lg font-semibold my-3 pl-2">{name}</h4>
       <h5 className="text-slate-500 flex pl-2  flex-wrap">
-        {cuisines.map((cusine, index) => (
+        {filteredCusines.map((cusine, index) => (
           <h4 className="" key={index}>
             {cusine},
           </h4>
